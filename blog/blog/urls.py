a98a5ec141ba89dbf55de34abfac5699f00f6a41
Olesya <progafox@gmail.com>
2022-06-14 17:22:53 +0500

    update

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
 
-from app.views import (PostAPIViews, PostSubscribeDetailViews,
-                       PostSubscribeListViews, SubscribeView, UserListViews)
+from app.views import (
+    PostAPIViews,
+    PostSubscribeDetailViews,
+    PostSubscribeListViews,
+    SubscribeView,
+    UserListViews,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
